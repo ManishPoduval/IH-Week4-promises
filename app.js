a1e5c7223c9087d3a5093b0e3f0ca6a34e88a3f4
Manish Poduval <manishpoduval@hotmail.com>
2020-05-28 14:11:46 +0530

    Starter express part 2

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ let app = express();
 //let express know what template engine you're using.
 app.set('view engine', 'hbs');
 
+//set the middleware to let express know where the static files are located
+app.use(express.static('public'))
+
+//define the routes
 app.get('/', (req, res) => {
-    res.send('Hey you\'re in landing page')
+    // .render always for a template engine
+    res.render(__dirname + '/views/landing.hbs');
 })
 
 app.get('/home', (req, res) => {
-    res.send('In home page')
+    res.render(__dirname + '/views/home.hbs');
 })
 
 app.get('/about', (req, res) => {
-    res.send('In about page!!!!!')
+    res.render(__dirname + '/views/about.hbs');
 })
 
+//create the server on port 3000
 app.listen('3000', () => {
     console.log(`Example app listening at http://localhost:3000`)
 })
